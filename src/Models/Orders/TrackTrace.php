fcc686702a9ff89c37d00c544946ccacad8cc36c
Peter <peter@afosto.com>
2018-05-29 10:25:41 +0200

    Added customer id to track trace

--- src/Models/Orders/TrackTrace.php
+++ src/Models/Orders/TrackTrace.php
@@ -13,6 +13,7 @@ use Afosto\FashionPartner\Components\Model;
  * @property string       $transporter
  * @property string       $boxNumber
  * @property string       $boxReference
+ * @property integer      $customer
  * @property ReturnItem[] $items
  */
 class TrackTrace extends Model {
